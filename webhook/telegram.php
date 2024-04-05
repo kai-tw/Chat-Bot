10a9543fbc52390bd106ce21d890e35d7913993c
Kai <wuvincentck505@gmail.com>
2024-04-06 01:00:12 +0800

    [Telegram] Add 400 Bad request

--- webhook/telegram.php
+++ webhook/telegram.php
@@ -2,13 +2,11 @@
 require_once '../vendor/autoload.php';
 require_once '../config.php';
 
-header('Content-Type: application/json; charset=utf-8');
-
 $telegram = new Telegram\Bot\Api(TELEGRAM_TOKEN);
 $response = $telegram->setWebhook(['url' => WEBHOOK_ROOT_PATH . '/telegram.php?token=' . TELEGRAM_TOKEN]);
 
 if (!isset($_GET['token']) || $_GET['token'] !== TELEGRAM_TOKEN) {
-    echo '{}';
+    header("HTTP/1.1 400 Bad Request.");
     exit(0);
 }
 
