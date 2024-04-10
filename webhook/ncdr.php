26837e1f0425172d1378c44d1f4f387703edaeed
Kai <wuvincentck505@gmail.com>
2024-04-11 01:25:52 +0800

    [Notify] Send to admin's account when receiving

--- webhook/ncdr.php
+++ webhook/ncdr.php
@@ -15,14 +15,24 @@ require_once '../class/user.php';
 require_once '../modules/ncdr/earthquake.php';
 
 ini_set('display_errors', FALSE);
-header('Content-Type: application/xml; charset=utf-8');
+header('Content-Type: text/xml; charset=utf-8');
 echo '<?xml version="1.0" encoding="utf-8" ?><Data><Status>true</Status></Data>';
 $url = 'php://input';
-// $url = 'https://alerts.ncdr.nat.gov.tw/Capstorage/CWA/2024/Earthquake/CWA-EQ113178-2024-0408-072850.cap';
+// $url = 'https://alerts.ncdr.nat.gov.tw/Capstorage/CWA/2024/Earthquake/CWA-EQ113192-2024-0411-005512.cap';
 $file = file_get_contents($url);
 if ($file !== '' && $file !== '<?xml version="1.0" encoding="utf-8"?><alert xmlns="urn:oasis:names:tc:emergency:cap:1.2"><Test>Test</Test></alert>') {
     $xml = new DOMDocument;
     $xml->load($url);
+
+    /**
+     * Telegram Api Initialization
+     */
+    $telegram = new Api(TELEGRAM_TOKEN);
+
+    $identifier = $xml->getElementsByTagName('identifier')[0]->nodeValue;
+    $sender = $xml->getElementsByTagName('sender')[0]->nodeValue;
+    $sent = $xml->getElementsByTagName('sent')[0]->nodeValue;
+    sendTelegramMessage($telegram, ADMIN_ACCOUNT, "{$identifier}\n{$sender}\n{$sent}");
 
     $messageList = [];
 
@@ -39,11 +49,6 @@ if ($file !== '' && $file !== '<?xml version="1.0" encoding="utf-8"?><alert xmln
         $lineConfig->setAccessToken(LINE_TOKEN);
         $messageApi = new MessagingApiApi($client, $lineConfig);
 
-        /**
-         * Telegram Api Initialization
-         */
-        $telegram = new Api(TELEGRAM_TOKEN);
-
         $db = new \mysqli(\DBHOST . ':' . \DBPORT, \DBUSER, \DBPASS, \DBNAME);
         $query = $db->query('SELECT usr.username, usr.line_id, usr.telegram_id FROM `users` usr INNER JOIN `ncdr_users` nusr ON usr.username = nusr.username WHERE nusr.earthquake = 1;');
         while ($item = $query->fetch_assoc()) {
@@ -59,8 +64,8 @@ if ($file !== '' && $file !== '<?xml version="1.0" encoding="utf-8"?><alert xmln
             sendLineMessage($messageApi, $item['line_id'], $message);
             sendTelegramMessage($telegram, $item['telegram_id'], $message);
         }
+        $db->close();
     }
-    $db->close();
 }
 
 function sendLineMessage(MessagingApiApi $messageApi, ?string $lineId, string $message)
