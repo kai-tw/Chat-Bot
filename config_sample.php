ec26d95752c78a5209231c6d745c0faf26c92ea9
Kai <wuvincentck505@gmail.com>
2024-04-27 23:09:02 +0800

    Add a sample config file

---
+++ config_sample.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Chat-Bot settings.
+ */
+define('WEBHOOK_ROOT_PATH', '');
+define('ADMIN_ACCOUNT', '');
+
+/**
+ * Tokens
+ */
+define('LINE_SECRETE', '');
+define('LINE_TOKEN', '');
+define('TELEGRAM_TOKEN', '');
+
+/**
+ * Database
+ */
+define('DBHOST', '');
+define('DBPORT', 3306);
+define('DBUSER', '');
+define('DBPASS', '');
+define('DBNAME', '');
